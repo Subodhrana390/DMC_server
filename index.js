7486ba97406b35bfcb9b9434b4726e875917b1bc
Subodh Rana <40374844+Subodhrana390@users.noreply.github.com>
2024-12-22 20:04:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.get("/health", (req, res) => {
 const keepAlive = () => {
   setInterval(async () => {
     try {
-      const res = await axios.get('https://dmc-server.onrender.com/health');
+      const res = await axios.get('https://dmc-server-h58l.onrender.com/health');
       console.log(`Self-ping success: ${res.data}`);
     } catch (error) {
       console.error('Self-ping failed:', error.message);
